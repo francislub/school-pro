7b15ed4ed6ab1c506c1e0866f329e678abcc847e
Eng Lubanjwa Francis <lubanjwafrancis@gmail.com>
2024-11-07 15:15:18 -0800

    more

---
+++ src/app/(back)/dashboard/layout.tsx
@@ -0,0 +1,6 @@
+import React , { ReactNode } from 'react';
+
+export default function DashboardLayout({ children }: {
+  children: ReactNode }) {
+    return <div>{ children }</div>;
+}
---
+++ src/app/(front)/layout.tsx
@@ -0,0 +1,6 @@
+import React , { ReactNode } from 'react';
+
+export default function FrontLayout({ children }: {
+  children: ReactNode }) {
+    return <div>{ children }</div>;
+}
